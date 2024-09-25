a09e8b6299983aeef6f0fc455513a075ccf20b87
Dante9l <zby99932@gmail.com>
2024-09-25 10:01:50 +0800

    lesson3

---
+++ example/lessonJava/lesPra31.java
@@ -0,0 +1,17 @@
+package lessonJava;
+
+public class lesPra31 {
+    String pinpai;
+    public lesPra31(String pinpai){
+        this.pinpai = pinpai;
+    }
+    public lesPra31(){}
+    public void charge() {
+        System.out.println(pinpai + "牌的电池充电中");
+    }
+
+    public static void main(String[] args) {
+        lesPra31 les = new lesPra31("南孚");
+        les.charge();
+    }
+}
---
+++ example/lessonJava/lesPra32.java
@@ -0,0 +1,26 @@
+package lessonJava;
+
+public class lesPra32 extends lesPra31{
+    String pinpai;
+    //子类重写父类方法
+    public void downloadMusic(){
+        System.out.println("下载音乐");
+    }
+    public void download(){
+        System.out.println("下载中");
+    }
+    public lesPra32(String pinpai) {
+        super(pinpai);
+        this.pinpai = pinpai;
+    }
+    public void change(){
+        super.charge();
+    }
+
+    public static void main(String[] args) {
+        lesPra32 lesPra32 = new lesPra32("华为");
+        lesPra32.downloadMusic();
+        lesPra32.download();
+        lesPra32.change();
+    }
+}
